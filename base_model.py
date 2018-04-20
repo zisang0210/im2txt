719757f3b61999105bdd7a5de0470625759adcd9
zisang <467862440@qq.com>
2018-04-20 20:11:18 +0800

    remove cv2

--- base_model.py
+++ base_model.py
@@ -11,7 +11,7 @@ import json
 from utils.nn import NN
 # from utils.coco.coco import COCO
 # from utils.coco.pycocoevalcap.eval import COCOEvalCap
-from utils.misc import ImageLoader, CaptionData, TopN
+from utils.misc import CaptionData, TopN
 
 class BaseModel(object):
     def __init__(self, config, mode="train"):
