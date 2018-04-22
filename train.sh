af49d12789a15224f4ad45a78ca83bfd8cfdfc58
zisang <467862440@qq.com>
2018-04-22 09:59:57 +0800

    for experiment

--- train.sh
+++ train.sh
@@ -1,9 +1,9 @@
 
 for $att in "fc1 fc2 bias bias2 bias_fc1 bias_fc2 rnn";
 do
-python train.py --input_file_pattern=../data/flickr8k/train-?????-of-00016 \
+python train.py --input_file_pattern='../data/flickr8k/train-?????-of-00016' \
     --number_of_steps=30 \    
-    --attention=$att \    
+    --attention='${att}' \    
     --optimizer='Adam' \    
-    --train_dir=../output/model/$att_Adam_30;
+    --train_dir='../output/model/${att}_Adam_10000';
 done
