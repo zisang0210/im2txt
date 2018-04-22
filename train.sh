a10c87f269e7f9b42a47829367e72848eb397141
zisang <467862440@qq.com>
2018-04-22 10:01:10 +0800

    for experiment

--- train.sh
+++ train.sh
@@ -1,5 +1,5 @@
 
-for $att in "fc1 fc2 bias bias2 bias_fc1 bias_fc2 rnn";
+for att in "fc1 fc2 bias bias2 bias_fc1 bias_fc2 rnn";
 do
 python train.py --input_file_pattern='../data/flickr8k/train-?????-of-00016' \
     --number_of_steps=30 \    
