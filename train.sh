2a832d026834a68c8d4c839421b17f3e19c34e4a
zisang <467862440@qq.com>
2018-04-22 10:05:55 +0800

    for experiment

--- train.sh
+++ train.sh
@@ -1,9 +1,5 @@
 
 for att in "fc1 fc2 bias bias2 bias_fc1 bias_fc2 rnn";
 do
-python train.py --input_file_pattern='../data/flickr8k/train-?????-of-00016' \
-    --number_of_steps=30 \    
-    --attention='${att}' \    
-    --optimizer='Adam' \    
-    --train_dir='../output/model/${att}_Adam_10000';
+python train.py --input_file_pattern='../data/flickr8k/train-?????-of-00016' --number_of_steps=30 --attention='${att}' --optimizer='Adam' --train_dir='../output/model/${att}_Adam_30';
 done
