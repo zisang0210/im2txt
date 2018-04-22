4c614b50ff8cbf02f80a5500831ac60dd401d85a
zisang <467862440@qq.com>
2018-04-22 12:12:56 +0800

    fix

--- train.sh
+++ train.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-ATTENTION="fc1 fc2 bias bias2 bias_fc1 bias_fc2"
+ATTENTION="fc1 fc2 bias bias2 bias_fc1 bias_fc2 rnn"
+NUMBER_OF_STEPS=30
 
 for att in $ATTENTION;
 do
-  python train.py --input_file_pattern="../data/flickr8k/train-?????-of-00016" \
-  --number_of_steps=30 \
+  echo python train.py --input_file_pattern="../data/flickr8k/train-?????-of-00016" \
+  --number_of_steps=$NUMBER_OF_STEPS \
   --attention="${att}" \
   --optimizer="Adam" \
-  --train_dir="../output/model/${att}_Adam_30";
+  --train_dir="../output/model/${att}_Adam_${NUMBER_OF_STEPS}";
 done
