761bcfd39ec88ec945eb5dce4df3891dcef7dd8b
zisang <467862440@qq.com>
2018-05-01 14:40:47 +0800

    add cmd

--- train.sh
+++ train.sh
@@ -11,3 +11,14 @@ do
   --optimizer="Adam" \
   --train_dir="../output/model/${att}_Adam_${NUMBER_OF_STEPS}";
 done
+
+
+python train.py --input_file_pattern="../data/flickr8k/train-?????-of-00016" \
+  --number_of_steps=120000 \
+  --attention="bias" \
+  --optimizer="Momentum" \
+  --momentum=0.9 \
+  --initial_learning_rate=0.001 \
+  --learning_rate_decay_factor=0.1 \
+  --num_steps_per_decay=40000 \
+  --train_dir="../output/model/bias_Momentum_lr_0.0005_decay_0.2_40000"
