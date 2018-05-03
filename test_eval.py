798e58c60e322b0e6f3fe78fbbe02c96374dce72
zisang <467862440@qq.com>
2018-05-03 17:45:55 +0800

    test

--- test_eval.py
+++ test_eval.py
@@ -217,9 +217,9 @@ def evaluate_model(sess, model, vocab, global_step, summary_writer):
         words_and_scores.sort(key=lambda x: -x[1])
         words_and_scores = words_and_scores[0]
         word = words_and_scores[0]
-        print(vocab.id_to_word(word),word,gts[b,cap_len])
+        # print(vocab.id_to_word(word),word,gts[b,cap_len])
 
-        last_word = word* np.ones((model.config.batch_size), np.int32)
+        last_word = gts[b,cap_len]* np.ones((model.config.batch_size), np.int32)
         last_memory = memory
         last_output = output
 
